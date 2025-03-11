b9c0872830634213d56cb07392b03e3122cd9b70
compactcoders <renusrisivakumar100@gmail.com>
2025-03-12 00:00:36 +0530

    styling changess

--- frontend/src/components/Coreteam.jsx
+++ frontend/src/components/Coreteam.jsx
@@ -20,32 +20,32 @@ import divya from "../assets/Divya.png";
 
 const CoreTeam = () => {
   const officeBearers = [
-    { name: "Benita Grace", role: "Chair", tagline: "Tagline", imageUrl: benita, linkedinUrl: "https://www.linkedin.com/in/benita-grace-966184253", instagramUrl: "https://www.instagram.com/benita.grace18" },
-    { name: "Hanna Elsa Jaiju", role: "Vice Chair", tagline: "Tagline", imageUrl: hanna, linkedinUrl: "https://www.linkedin.com/in/hannaelsajaiju" },
-    { name: "Kalaipriya S", role: "Secretary", tagline: "Tagline", imageUrl: kalai, linkedinUrl:"http://www.linkedin.com/in/kalaipriya-suresh-270b55287", instagramUrl: "https://www.instagram.com/kalai._.priya" },
-    { name: "Jeyasri KS", role: "Treasurer", tagline: "Tagline", imageUrl: jeyasri, linkedinUrl:"https://www.linkedin.com/in/jeyasri-ks-1310b8288", instagramUrl: "https://www.instagram.com/_.jeyasri28._" },
+    { name: "Benita Grace", role: "Chair", tagline: "Transforming the landscape through innovative leadership", imageUrl: benita, linkedinUrl: "https://www.linkedin.com/in/benita-grace-966184253", instagramUrl: "https://www.instagram.com/benita.grace18" },
+    { name: "Hanna Elsa Jaiju", role: "Vice Chair", tagline: "Where vision meets strategic direction", imageUrl: hanna, linkedinUrl: "https://www.linkedin.com/in/hannaelsajaiju" },
+    { name: "Kalaipriya S", role: "Secretary", tagline: "The Keeper of the Narrative", imageUrl: kalai, linkedinUrl:"http://www.linkedin.com/in/kalaipriya-suresh-270b55287", instagramUrl: "https://www.instagram.com/kalai._.priya" },
+    { name: "Jeyasri KS", role: "Treasurer", tagline: "Where financial expertise meets strategic vision", imageUrl: jeyasri, linkedinUrl:"https://www.linkedin.com/in/jeyasri-ks-1310b8288", instagramUrl: "https://www.instagram.com/_.jeyasri28._" },
   ];
 
   const coreMembers = {
     Management: [
-      { name: "Kavirithanya", role: "Management Lead", tagline: "Tagline", imageUrl: kavirithanya, linkedinUrl:"https://www.linkedin.com/in/kavirithanya-52093a251", instagramUrl:"https://www.instagram.com/kavirithanya" },
-      { name: "Freneta", role: "Management Co-lead", tagline: "Tagline", imageUrl: freneta, linkedinUrl:"https://www.linkedin.com/in/freneta-amalraj-11bab72a8", instagramUrl: "https://www.instagram.com/freneta_fries" },
+      { name: "Kavirithanya", role: "Management Lead", tagline: "Executing plans with precision and purpose", imageUrl: kavirithanya, linkedinUrl:"https://www.linkedin.com/in/kavirithanya-52093a251", instagramUrl:"https://www.instagram.com/kavirithanya" },
+      { name: "Freneta", role: "Management Co-lead", tagline: "Fueling the fire of innovation", imageUrl: freneta, linkedinUrl:"https://www.linkedin.com/in/freneta-amalraj-11bab72a8", instagramUrl: "https://www.instagram.com/freneta_fries" },
     ],
     Technical: [
-      { name: "Renusri", role: "Technical Lead", tagline: "Tagline", imageUrl: renusri, linkedinUrl: "https://www.linkedin.com/in/renusri-s-413938246", instagramUrl: "https://www.instagram.com/renusris_01" },
-      { name: "Priyadarshini", role: "Technical Co-lead", tagline: "Tagline", imageUrl: priya, linkedinUrl: "http://www.linkedin.com/in/priyadharshini-r-9a756a288", instagramUrl: "https://www.instagram.com/prrriiiyaaa_" },
+      { name: "Renusri", role: "Technical Lead", tagline: "Crafting vision that brings ideas to life.", imageUrl: renusri, linkedinUrl: "https://www.linkedin.com/in/renusri-s-413938246", instagramUrl: "https://www.instagram.com/renusris_01" },
+      { name: "Priyadarshini", role: "Technical Co-lead", tagline: "Design solutions that inspire and elevate.", imageUrl: priya, linkedinUrl: "http://www.linkedin.com/in/priyadharshini-r-9a756a288", instagramUrl: "https://www.instagram.com/prrriiiyaaa_" },
     ],
     Content: [
-      { name: "Virutika", role: "Content Lead", tagline: "Tagline", imageUrl: virutika, linkedinUrl: "https://www.linkedin.com/in/virutika-pugazhendhi-7699482a0", instagramUrl: "https://www.instagram.com/_.virutika._" },
-      { name: "Chandra", role: "Content Co-lead", tagline: "Tagline", imageUrl: chandra, linkedinUrl: "https://www.linkedin.com/in/sashritha-chandra-8a9586319", instagramUrl: "https://www.instagram.com/sashritha34" },
+      { name: "Virutika", role: "Content Lead", tagline: "The architect of innovative solutions in tech", imageUrl: virutika, linkedinUrl: "https://www.linkedin.com/in/virutika-pugazhendhi-7699482a0", instagramUrl: "https://www.instagram.com/_.virutika._" },
+      { name: "Chandra", role: "Content Co-lead", tagline: "Pioneering innovative solutions, one code at a time", imageUrl: chandra, linkedinUrl: "https://www.linkedin.com/in/sashritha-chandra-8a9586319", instagramUrl: "https://www.instagram.com/sashritha34" },
     ],
     Design: [
-      { name: "Janapriya", role: "Design Lead", tagline: "Tagline", imageUrl: janapriya, linkedinUrl: "https://www.linkedin.com/in/janapriya-s-11j01" },
-      { name: "Paveethra", role: "Design Co-lead", tagline: "Tagline", imageUrl: paveethra, linkedinUrl: "https://www.linkedin.com/in/pavethraa09", instagramUrl: "https://www.instagram.com/invites/contact" },
+      { name: "Janapriya", role: "Design Lead", tagline: "Transforming ideas into engaging content", imageUrl: janapriya, linkedinUrl: "https://www.linkedin.com/in/janapriya-s-11j01" },
+      { name: "Paveethra", role: "Design Co-lead", tagline: "Crafting compelling stories that resonate", imageUrl: paveethra, linkedinUrl: "https://www.linkedin.com/in/pavethraa09", instagramUrl: "https://www.instagram.com/invites/contact" },
     ],
     Media: [
-      { name: "Koushika", role: "Media Lead", tagline: "Tagline", imageUrl: koushika, linkedinUrl: "https://www.linkedin.com/in/koushika-ramesh-b70a78315" },
-      { name: "Dhanashree", role: "Media Co-lead", tagline: "Tagline", imageUrl: dhanashree, linkedinUrl: "https://www.linkedin.com/in/dhanashree-elumalai-436b55311", instagramUrl: "https://www.instagram.com/dhanucore._" },
+      { name: "Koushika", role: "Media Lead", tagline: "The intersection of media and creativity", imageUrl: koushika, linkedinUrl: "https://www.linkedin.com/in/koushika-ramesh-b70a78315" },
+      { name: "Dhanashree", role: "Media Co-lead", tagline: "The art of storytelling, perfected.", imageUrl: dhanashree, linkedinUrl: "https://www.linkedin.com/in/dhanashree-elumalai-436b55311", instagramUrl: "https://www.instagram.com/dhanucore._" },
     ],
   };
 
@@ -57,9 +57,9 @@ const CoreTeam = () => {
   return (
     <section className="core-team">
       <h1>Core Team</h1>
-      <p>The core team of ACM-W at Sathyabama University is a group of passionate, dedicated individuals focused on empowering women 
-        in the fields of computing and technology. Our mission is to foster an inclusive and collaborative environment where women can 
-        grow, innovate, and lead.</p>
+      <p>Meet the Core Team of ACM-W Sathyabama
+Our core team is comprised of talented and motivated students who share a passion for technology and empowering women in computing. Led by a team of dedicated leaders, including our executive council and team leads, they work collaboratively to drive ACM-W's mission forward. With diverse skills and expertise, our core team members are the driving force behind our events, initiatives, and community-building efforts, striving to create a supportive and inclusive environment for all members.
+</p>
 
       <h2>Office Bearers</h2>
       <div className="office-bearers">
