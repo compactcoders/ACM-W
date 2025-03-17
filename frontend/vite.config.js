8485e0f47841d3105b0a84859c0fa325340678cd
compactcoders <renusrisivakumar100@gmail.com>
2025-03-17 23:22:17 +0530

    acm member page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Replace "your-username" and "ACM-W" with your actual GitHub username and repo name
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/ACM-W/", // Ensure the correct repository name is set
+});
